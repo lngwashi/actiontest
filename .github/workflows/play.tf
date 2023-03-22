49dec77abe02cc656f5b3d92aebfd7717eb14324
lngwashi <lengwash@gmail.com>
2023-03-22 16:52:47 -0400

    Create play.tf

---
+++ .github/workflows/play.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "actions" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Class30"
+    Team ="DevOps"
+    Division="HR"
+  }
+}
