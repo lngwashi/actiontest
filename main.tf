2c9948dd36c12bfa253d1fe6835c8c489ba420fe
lngwashi <lengwash@gmail.com>
2023-04-02 11:46:57 -0400

    fourth commit message

--- main.tf
+++ main.tf
@@ -31,39 +31,3 @@ resource "aws_vpc" "actions" {
     Division="HR"
   }
 }
-
-resource "aws_instance" "bootcamp31" {
-     #ami           = "ami-0e5b6b6a9f3db6db8" # Amazon Linux
-     ami = data.aws_ami.ubuntu.id
-     instance_type = var.instance_type[1]
-     delete_on_termination = true
-
-     tags = {
-      Name = local.name
-     }
-    }
-
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  type = list(string)
-  default = ["t2.micro", "t2.medium"]
-}
-
-output "public_ip" {
-  description = "ec2 instance public ip"
-  value = aws_instance.inst1.arn
-}
-
-locals {
-    name = "${var.app_name}-${var.environment}"
-  }
-  
- data "aws_ami" "ubuntu" {
-     most_recent = true
-     owners = ["self"]
-
-     filter {
-      name = "name"
-      value = ["packer-docker"]
-     }
-   } 
