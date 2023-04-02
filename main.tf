34609576bb50f5b439be1c8d92439e2f8f470b50
lngwashi <lengwash@gmail.com>
2023-04-02 11:54:24 -0400

    fourth commit message

--- main.tf
+++ main.tf
@@ -1,29 +1,9 @@
-terraform {
-  required_version = "~> 1.0" 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  /*
-  backend "s3" {
-    bucket = "terraform-mylandmark"
-    key    = "prod/terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-lock"
-  }
-  */
-}
-
-
-
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_vpc" "actions" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "Class30"
