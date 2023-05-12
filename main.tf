d46fd788211f491887ef689a05ce8ae9d66509ea
lngwashi <lengwash@gmail.com>
2023-05-12 14:00:58 -0400

    terraform plan

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 # Configure the AWS Provider
 provider "aws" {
-  version = "~> 4.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "actions" {
